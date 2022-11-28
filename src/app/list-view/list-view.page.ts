1414fa42a60620d8ba720b22847d77c18a20d4ad
Komal Shah <komshah@WKMZTB7B8DFB.global.publicisgroupe.net>
2022-11-28 22:13:47 +0530

    Adding async pipe

--- src/app/list-view/list-view.page.ts
+++ src/app/list-view/list-view.page.ts
@@ -2,36 +2,23 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { Employee } from '../models/employee.model';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.page.html',
   styleUrls: ['./list-view.page.scss'],
 })
-export class ListViewPage implements OnInit, OnDestroy {
-  employeeDataList: Employee[] = [];
-  employeeListSubs: Subscription = new Subscription();
+export class ListViewPage implements OnInit {
+  employeeDataList$!: Observable<Employee[]>;
 
   constructor(private router: Router, private dataService: DataService) {}
 
   ngOnInit() {
-    this.getEmployeeList();
-  }
-
-  getEmployeeList() {
-    this.employeeListSubs = this.dataService
-      .fetchData()
-      .subscribe((employeeList: Employee[]) => {
-        this.employeeDataList = employeeList;
-      });
+    this.employeeDataList$ = this.dataService.fetchData();
   }
 
   displayEmployeeDetails(data: { id: Number }) {
     this.router.navigate(['detail-view', data.id]);
-  }
-
-  ngOnDestroy() {
-    this.employeeListSubs.unsubscribe();
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,9 @@ import { Observable } from 'rxjs';
 export class DataService {
   constructor(private http: HttpClient) {}
 
-  fetchData(): Observable<any> {
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  fetchData(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(
+      'https://jsonplaceholder.typicode.com/users'
+    );
   }
 }
