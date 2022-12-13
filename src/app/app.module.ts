bc121a6dfe429adf347fb5851d0c6e18a452ae72
Komal Shah <komshah@WKMZTB7B8DFB.global.publicisgroupe.net>
2022-12-13 21:43:52 +0530

    Added custom splash screen and added cordova-res for generating resources for splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+//import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/list-view/list-view.page.ts
+++ src/app/list-view/list-view.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { Employee } from '../models/employee.model';
